5f3c6a183114a62eafa8f86a607326a81d573b9e
Felix Brandstetter <Felix.Brandstetter@gmx.de>
2024-01-09 10:22:39 +0100

    Added Structure folders

---
+++ evaluation/evaluation_metrics/precision_recall/main.py
@@ -0,0 +1,8 @@
+# Idee zwei json annhemen
+# Strings vergleichen
+# Bei anderen Strings von User bewerten lassen ob partial correct
+# Precision und recall berechnen
+
+#Partial, wenn typ nicht übereinstimmt
+
+#erstellen einer Datei für wiederverwendbarkeit
